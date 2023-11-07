8ae5484ece8b1d3584dcc00d3a4ff25a33d265b2
garrett <garrett.txt@gmail.com>
2023-11-07 07:03:47 -0600

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
+import { Analytics } from '@vercel/analytics/react'
+import { RootLayout } from '@/components/RootLayout'
 
-  import { RootLayout } from '@/components/RootLayout'
-
-  import '@/styles/tailwind.css'
+import '@/styles/tailwind.css'
 import { Metadata } from 'next'
 import Head from 'next/head'
 
@@ -9,7 +9,10 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="h-full bg-neutral-950 text-base antialiased">
       <body className="flex min-h-full flex-col">
-        <RootLayout>{children}</RootLayout>
+        <RootLayout>
+          {children}
+          <Analytics />
+        </RootLayout>
       </body>
     </html>
   )
