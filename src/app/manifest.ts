776fefd1db42caafa9a329b86a20786f950faa5e
Garrett Weems <gwgraphicdesign@gmail.com>
2025-08-08 21:54:21 -0500

    fix: update theme color to black and remove unused Open Graph image component

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -10,7 +10,7 @@ export default function manifest(): MetadataRoute.Manifest {
     start_url: '/',
     display: 'standalone',
     background_color: '#fff',
-    theme_color: '#fff',
+    theme_color: '#000',
     icons: [
       {
         src: '/favicon.ico',
